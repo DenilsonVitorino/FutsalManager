0eae70b736c14e70987ec7d2403d6e67c6977d5f
Denilson Vitorino <dv22pro@gmail.com>
2020-10-17 17:03:31 -0300

    novas modificacoes no splash screen

--- app/src/main/java/br/com/futsalmanager/Adapter/HorarioAdapter.java
+++ app/src/main/java/br/com/futsalmanager/Adapter/HorarioAdapter.java
@@ -1,10 +1,15 @@
 package br.com.futsalmanager.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.util.List;
@@ -12,8 +17,11 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 import br.com.futsalmanager.R;
+import br.com.futsalmanager.controller.HorarioController;
 import br.com.futsalmanager.model.Horario;
 import br.com.futsalmanager.util.NumberFormat;
+import br.com.futsalmanager.view.HorarioCadView;
+import br.com.futsalmanager.view.HorarioView;
 
 public class HorarioAdapter extends RecyclerView.Adapter<HorarioAdapter.MyViewHolder> {
 
@@ -48,10 +56,36 @@ public class HorarioAdapter extends RecyclerView.Adapter<HorarioAdapter.MyViewHo
                 "\nHorário: " + mData.get(i).getHora()
 
         );
-        myViewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+        myViewHolder.btHorarioCardEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(mContext, HorarioCadView.class);
+                Bundle bundle = new Bundle();
+                bundle.putLong("id",mData.get(i).getId());
+                intent.putExtras(bundle);
+                mContext. startActivity(intent);
+            }
+        });
 
+        myViewHolder.btHorarioCardDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setMessage("Deseja remover horário?")
+                        .setCancelable(false)
+                        .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                new HorarioController(mContext).deletaDado(mData.get(i).getId());
+                                HorarioView.alimentarLista();
+                            }
+                        })
+                        .setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
             }
         });
     }
@@ -66,12 +100,17 @@ public class HorarioAdapter extends RecyclerView.Adapter<HorarioAdapter.MyViewHo
         CardView cardView;
         TextView lbHorarioTexto;
         LinearLayout llHorarioList;
+        Button btHorarioCardEdit,
+               btHorarioCardDelete;
+
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             lbHorarioTexto = (TextView) itemView.findViewById(R.id.lbHorarioTexto);
             cardView = (CardView) itemView.findViewById(R.id.horario_card);
             llHorarioList = (LinearLayout) itemView.findViewById(R.id.llHorarioList);
+            btHorarioCardEdit = (Button) itemView.findViewById(R.id.btHorarioCardEdit);
+            btHorarioCardDelete = (Button) itemView.findViewById(R.id.btHorarioCardDelete);
         }
     }
 }
--- app/src/main/java/br/com/futsalmanager/view/SplashScreen.java
+++ app/src/main/java/br/com/futsalmanager/view/SplashScreen.java
@@ -1,7 +1,12 @@
 package br.com.futsalmanager.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import br.com.futsalmanager.R;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(),HorarioView.class));
+                finish();
+            }
+        },3000);
     }
 }
